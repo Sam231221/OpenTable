da301c45d378aa28c0ff38f8fa53d78469f79195
Sam231221 <samirshahi9882@gmail.com>
2023-06-01 11:25:27 +0545

    Files up to date

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import NavBar from "./components/NavBar";
+import AuthContext from "./context/AuthContext";
+import "./globals.css";
+import "react-datepicker/dist/react-datepicker.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <main className="bg-gray-100 min-h-screen w-screen">
+          <AuthContext>
+            <main className="max-w-screen-2xl m-auto bg-white">
+              <NavBar />
+              {children}
+            </main>
+          </AuthContext>
+        </main>
+      </body>
+    </html>
+  );
+}
