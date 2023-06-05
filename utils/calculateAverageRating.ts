497ebccf609526b2b76192d6a5da4e795abe81e7
Sam231221 <samirshahi9882@gmail.com>
2023-06-05 21:59:23 +0545

    Emptyed project

--- utils/calculateAverageRating.ts
+++
@@ -1,7 +0,0 @@
-import { Review } from '@prisma/client';
-export const calculateAverageRating =(reviews:Review[])=>{
-    if (!reviews?.length) return 0;
-    return reviews.reduce((sum, review)=>{
-        return sum +review.rating
-    }, 0)/reviews.length
-}
